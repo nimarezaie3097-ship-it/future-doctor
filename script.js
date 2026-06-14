e4b39336f64dc410ca62b448377c4de54375d3e4
nimarezaie3097-ship-it <nimarezaie3097@gmail.com>
2026-06-14 18:30:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function scrollToSection(id){
         behavior:"smooth"
     });
 }
+const hiddenElements =
+document.querySelectorAll('.section');
+
+const observer =
+new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+hiddenElements.forEach(el=>{
+    el.classList.add('hidden');
+    observer.observe(el);
+});
